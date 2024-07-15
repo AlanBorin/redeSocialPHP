1eaeb3db84509b66a8427ddc7dbc4bcdd57cdb4f
alanborin4 <alanborin4@gmail.com>
2024-07-14 23:20:12 -0300

    ajuste seguir e curtir

--- back/seguir.php
+++ back/seguir.php
@@ -1,16 +1,21 @@
 <?php
-require '../back/config.php';
-require '../back/perfilService.php';
+require 'config.php';
+require 'perfilService.php';
 
-session_start();
+autenticar();
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST['seguido_id'])) {
-        $seguido_id = $_POST['seguido_id'];
-        $seguidor_id = $_SESSION['id'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $seguido_id = $_POST['seguido_id'];
+    $seguidor_id = $_SESSION['id'];
+
+    $segue = verificaSeUsuarioSegue($db, $seguidor_id, $seguido_id);
+
+    if ($segue) {
+        deixarDeSeguirUsuario($db, $seguidor_id, $seguido_id);
+        echo json_encode(['status' => 'nao_seguido']);
+    } else {
         seguirUsuario($db, $seguidor_id, $seguido_id);
-        header('Location: ../front/index.php');
-        exit;
+        echo json_encode(['status' => 'seguido']);
     }
 }
 ?>
